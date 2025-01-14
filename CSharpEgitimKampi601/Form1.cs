f21f1d469f36663455efa46f615cfd968c1a3bd9
rumeysavarli23@gmail.com <rumeysavarli23@gmail.com>
2025-01-15 00:18:41 +0300

    Add project files.

---
+++ CSharpEgitimKampi601/Form1.cs
@@ -0,0 +1,72 @@
+﻿using CSharpEgitimKampi601.Entities;
+using CSharpEgitimKampi601.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        CustomerOperations customerOperations = new CustomerOperations();
+        private void btnCustomerCreat_Click(object sender, EventArgs e)
+        {
+            var customer = new Customer()
+            {
+                CustomerName = txtCustomerName.Text,
+                CustomerSurname = txtCustomerSurname.Text,
+                CustomerCity = txtCustomerCity.Text,
+                CustomerBalance =decimal.Parse(txtCustomerBalance.Text),
+                CustomerShoppingCount = int.Parse(txtCustomerShoppingCount.Text)
+            };
+            customerOperations.AddCustomer(customer);
+            MessageBox.Show("Müşteri ekleme işlemi başarılı","Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnCustomerList_Click(object sender, EventArgs e)
+        {
+            List<Customer> customers = customerOperations.GetAllCustomers();
+            dataGridView1.DataSource = customers; 
+        }
+
+        private void btnCustomerDelete_Click(object sender, EventArgs e)
+        {
+            string customerId=txtCustomerId.Text;
+            customerOperations.DeleteCustomer(customerId);
+            MessageBox.Show("Müşteri başarıyla silimdi");
+        }
+
+        private void btnCustomerUpdate_Click(object sender, EventArgs e)
+        {
+            string id =txtCustomerId.Text;
+            var updateCustomer = new Customer()
+            {
+                CustomerName = txtCustomerName.Text,
+                CustomerBalance = decimal.Parse(txtCustomerBalance.Text),
+                CustomerCity = txtCustomerCity.Text,
+                CustomerShoppingCount = int.Parse(txtCustomerShoppingCount.Text),
+                CustomerSurname = txtCustomerSurname.Text,
+                CustomerId=id
+            };
+            customerOperations.UpdateCustomer(updateCustomer);
+            MessageBox.Show("Müşteri başarıyla güncellendi");
+        }
+
+        private void btnGetCustomerById_Click(object sender, EventArgs e)
+        {
+            string id= txtCustomerId.Text;
+            Customer customer = customerOperations.GetCustomerById(id);
+            dataGridView1.DataSource= new List<Customer> { customer };
+        }
+    }
+}
---
+++ CSharpEgitimKampi601/Services/CustomerOperations.cs
@@ -0,0 +1,95 @@
+﻿using CSharpEgitimKampi601.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi601.Services
+{
+    public class CustomerOperations
+    {
+        public void AddCustomer(Customer customer)
+        {
+            var connection = new MongoDbConnection();
+            var customerCollection = connection.GetCustomersCollection();
+
+            var document = new BsonDocument
+            {
+                {"CustomerName" , customer.CustomerName},
+                {"CustomerSurname" , customer.CustomerSurname},
+                {"CustomerCity" , customer.CustomerCity},
+                {"CustomerBalance" , customer.CustomerBalance},
+                {"CustomerShoppingCount" , customer.CustomerShoppingCount}
+            };
+            customerCollection.InsertOne(document);
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            {
+                var connection = new MongoDbConnection();
+                var customerCollection = connection.GetCustomersCollection();
+                var customers = customerCollection.Find(new BsonDocument()).ToList();
+                List <Customer> customerList = new List<Customer>();
+                foreach (var c in customers)
+                {
+                    customerList.Add(new Customer
+                    {
+                        CustomerId = c["_id"].ToString(),
+                        CustomerName = c["CustomerName"].ToString(),
+                        CustomerSurname = c["CustomerSurname"].ToString(),
+                        CustomerBalance = decimal.Parse(c["CustomerBalance"].ToString()),
+                        CustomerCity = c["CustomerCity"].ToString(),
+                        CustomerShoppingCount = int.Parse(c["CustomerShoppingCount"].ToString())
+                    });
+                }
+                return customerList;
+            }
+        }
+
+        public void DeleteCustomer(string id)
+        {
+            var connection = new MongoDbConnection();
+            var customersCollection = connection.GetCustomersCollection();
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+            customersCollection.DeleteOne(filter);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            var connection = new MongoDbConnection();
+            var customerCollection= connection.GetCustomersCollection();
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(customer.CustomerId));
+            var updateValue = Builders<BsonDocument>.Update
+                .Set("CustomerName", customer.CustomerName)
+                .Set("CustomerSurname", customer.CustomerSurname)
+                .Set("CustomerCity", customer.CustomerCity)
+                .Set("CustomerBalance", customer.CustomerBalance)
+                .Set("CustomerShoppingCount", customer.CustomerShoppingCount);
+            customerCollection.UpdateOne(filter, updateValue);
+
+
+
+        }
+
+        public Customer GetCustomerById(string id)
+        {
+            var connection = new MongoDbConnection();
+            var customerCollection=connection.GetCustomersCollection();
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(id));
+            var result=customerCollection.Find(filter).FirstOrDefault();
+            return new Customer
+            {
+                CustomerBalance = decimal.Parse(result["CustomerBalance"].ToString()),
+                CustomerCity = result["CustomerCity"].ToString(),
+                CustomerId = id,
+                CustomerName = result["CustomerName"].ToString(),
+                CustomerShoppingCount = int.Parse(result["CustomerShoppingCount"].ToString()),
+                CustomerSurname = result["CustomerSurname"].ToString()
+            };
+        }
+    }
+}
